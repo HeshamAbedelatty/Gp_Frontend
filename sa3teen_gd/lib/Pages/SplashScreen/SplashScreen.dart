c77a304c25f21c0d0abc2fd97871290327ed7690
SalmaMourad <salmamourad992@gmail.com>
2024-06-23 00:26:51 +0300

    entering SplashScreen

---
+++ sa3teen_gd/lib/Pages/SplashScreen/SplashScreen.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, '/login');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'lib/assets/icons/treeCupAltered.png',  // Ensure you have the logo image in your assets
+              height: 150.0,
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              'Sa3teen Gd',  // Replace with your app name
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,  // Green color for the app name
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Now Studying can be Easier...',
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.black,  // Green color for the slogan
+              ),
+            ),
+            SizedBox(height: 50.0),
+            SpinKitCircle(
+              color: Colors.green,  // Green color for the loading spinner
+              size: 50.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_spinkit/flutter_spinkit.dart';
+// import 'dart:async';
+
+// import 'package:gp_screen/widgets/constantsAcrossTheApp/constants.dart';
+
+// class SplashScreen extends StatefulWidget {
+//   @override
+//   _SplashScreenState createState() => _SplashScreenState();
+// }
+
+// class _SplashScreenState extends State<SplashScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     Timer(const Duration(seconds: 2), () {
+//       Navigator.pushReplacementNamed(context, '/login');
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: kprimaryColourWhite,
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Image.asset(
+//               'lib/assets/icons/treeCupAltered.png',  // Ensure you have the logo image in your assets
+//               height: 150.0,
+//             ),
+//           const  SizedBox(height: 20.0),
+//            const Text(
+//               'Sa3teen Gd',  // Replace with your app name
+//               style: TextStyle(
+//                 fontSize: 30.0,
+//                 fontWeight: FontWeight.bold,
+//                 color: Colors.black,  // Green color for the app name
+//               ),
+//             ),
+//            const SizedBox(height: 10.0),
+//            const Text(
+//               'Now Studying can be Easier...',
+//               style: TextStyle(
+//                 fontSize: 16.0,
+//                 color: Colors.black,  // Green color for the slogan
+//               ),
+//             ),
+//             const SizedBox(height: 50.0),
+//            const SpinKitCircle(
+//               color: Colors.green,  // Green color for the loading spinner
+//               size: 50.0,
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
---
+++ sa3teen_gd/lib/Pages/SplashScreen/mainsplash.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:gp_screen/Pages/SplashScreen/SplashScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MyAppName',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: SplashScreen(),   
+       //l mfrod t3ml navigate ll login bgd bs na m4 3ndy l backend f m4 ht48al -_-
+      routes: {
+        '/login': (context) => LoginPage(),  // Define the login page route
+      },
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Center(
+        child: Text('Login Page'),
+      ),
+    );
+  }
+}
